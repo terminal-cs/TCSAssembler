a214e1d5d200861bce6494fdceb45b8da2964f62
Evan Lawrence <mailing.terminal.cs@gmail.com>
2022-03-01 15:45:42 -0800

    initial code

---
+++ TCSAssembler/Kernel/Kernel.cs
@@ -0,0 +1,13 @@
+﻿using Console = TCSAssembler.Assembler.X86.Console;
+
+namespace TCSAssembler.Kernel
+{
+    public static class Kernel
+    {
+        public static void KernelEntry()
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("This was compiled from dotnet 6.0 c#!");
+        }
+    }
+}
---
+++ TCSAssembler/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TCSAssembler
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            Assembler.X86.CoreX86.Init();
+            Kernel.Kernel.KernelEntry();
+            Assembler.X86.CoreX86.Export("Kernel.asm");
+        }
+    }
+}
