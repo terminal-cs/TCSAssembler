b6ac761515b83a8f89bcde430db6f079361841bc
Evan Lawrence <mailing.terminal.cs@gmail.com>
2022-03-01 22:48:40 -0800

    minor changes

--- Kernel/Program.cs
+++ Kernel/Program.cs
@@ -5,6 +5,10 @@
         public static void Main()
         {
             Console.WriteLine("Hello, World!");
+            SetPixel();
         }
+
+        // We could do something like this for custom methods
+        public static extern void SetPixel();
     }
 }
