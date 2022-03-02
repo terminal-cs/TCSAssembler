7d84eb4d4d83b2a330d3f84980846a872246dac6
Astrido <73673156+asterd-og@users.noreply.github.com>
2022-03-02 18:13:15 -0300

    vars now work properly

--- TCSAssembler/Assembler/X86.cs
+++ TCSAssembler/Assembler/X86.cs
@@ -1,4 +1,4 @@
-﻿using dnlib.DotNet;
+using dnlib.DotNet;
 using dnlib.DotNet.Emit;
 
 namespace TCSAssembler.Assembler
@@ -7,71 +7,49 @@ namespace TCSAssembler.Assembler
     {
         public X86()
         {
-            ASM.Add("[org 0x7c00]");
-            ASM.Add("[bits 32]");
-            ASM.Add("jmp Source.Kernel.Main\n");
             Instructions.Add(Code.Ldstr, LoadString);
+            Instructions.Add(Code.Ret, Ret);
         }
 
-        /*
-        DB	Define Byte	allocates 1 byte
-        DW	Define Word	allocates 2 bytes
-        DD	Define Doubleword	allocates 4 bytes
-        DQ	Define Quadword	allocates 8 bytes
-        DT	Define Ten Bytes	allocates 10 bytes
-
-        EXAMPLE:
-        choice		DB	'y'
-        number		DW	12345
-        neg_number	DW	-12345
-        big_number	DQ	123456789
-        real_number1	DD	1.234
-        real_number2	DQ	123.456
-        */
-
-        public List<string> ASM = new();
-        public delegate void Method(MethodDef Method, Instruction Instruction);
-        private readonly Dictionary<Code, Method> Instructions = new();
-        public int VIndex;
+        public Dictionary<string, List<string>> ASM = new();
+        public delegate void Method(MethodDef Method, Instruction Instruction, int Index);
+        private Dictionary<Code, Method> Instructions=new();
+        public int VIndex=0;
+        public int SIndex=0;
 
         public void ParseMethod(MethodDef Method)
         {
             if (Method.Name == ".cctor")  // Ignore useless class constructor
                 return;
+            
+            string MethodName=GetMethodName(Method);
 
-            ASM.Add($"{GetMethodName(Method)}:");
-            if (Method.Body.Variables.Count > 0)
-            {
-                ASM.Add("\tpush ebp");
-                ASM.Add("\tmov ebp, esp");
+            ASM.Add($"{MethodName}", new());
+            if (Method.Body.Variables.Count>0) {
+                ASM[MethodName].Add("\tpush ebp");
+                ASM[MethodName].Add("\tmov  ebp, esp");
+                ASM[MethodName].Add("\t; /\\ We have variables!");
             }
-
             for (int CI = 0; CI < Method.Body.Instructions.Count; CI++)
             {
                 Instruction Instruction = Method.Body.Instructions[CI];
                 switch (Instruction.OpCode.Code)
                 {
-                    case Code.Ret:
-                        ASM.Add("\tret");
-                        continue;
                     case Code.Nop:
                         continue;
                     default:
                         if (Instructions.ContainsKey(Instruction.OpCode.Code))
-                            Instructions[Instruction.OpCode.Code].Invoke(Method, Instruction);
-                        ASM.Add($"\t; Instruction: {Instruction}");
+                            Instructions[Instruction.OpCode.Code].Invoke(Method, Instruction, CI);
+                        ASM[MethodName].Add($"\t; Instruction: {Instruction}");
                         break;
                 }
+                /*if (Instruction.OpCode.Code!=Code.Nop) {
+                    if (Instructions.ContainsKey(Instruction.OpCode.Code))
+                        Instructions[Instruction.OpCode.Code].Invoke(Method, Instruction, CI);
+                    ASM[MethodName].Add($"\t; Instruction: {Instruction}");
+                }*/
             }
-            VIndex = 0;
-            /*for (int i=0;i<method.Body.Variables.Count;i++) {
-                var type=method.Body.Variables[i].Type;
-                Console.WriteLine(type.ToString());
-                code.Add($"{method.DeclaringType.Namespace}.{method.Name}.{method.Body.Variables[i].Name}:");
-                code.Add($"\t.size dd {method.Body.Variables[i]}");
-                //code.Add($"; {method.Body.Variables[i].Name} type: {method.Body.Variables[i].Type}");
-                code.Add($"; {method.Body.Variables[i].Name} type: {method.Body.Variables[i].Type}");
-            }*/
+            VIndex=0;
         }
 
         public void ParseFields(TypeDef Type)
@@ -80,46 +58,86 @@ namespace TCSAssembler.Assembler
             {
                 if (variable.IsStatic)
                 {
-                    ASM.Add($"\t{GetFieldName(Type, variable)}: dq {(variable.HasConstant ? variable.Constant.Value + $" ;type: {variable.Constant.Type}" : 0)}");
+                    //ASM.Add($"\t{GetFieldName(Type, variable)}: dq {(variable.HasConstant ? variable.Constant.Value + $" ;type: {variable.Constant.Type}" : 0)}");
+                    ASM.Add(GetFieldName(Type, variable), new());
+                    ASM[GetFieldName(Type, variable)].Add($"\tdq {(variable.HasConstant ? variable.Constant.Value + $" ;type: {variable.Constant.Type}" : 0)}");
                     //format the name, to make it readable
+                    /*
+                    DB	Define Byte	allocates 1 byte
+                    DW	Define Word	allocates 2 bytes
+                    DD	Define Doubleword	allocates 4 bytes
+                    DQ	Define Quadword	allocates 8 bytes
+                    DT	Define Ten Bytes	allocates 10 bytes
+
+                    EXAMPLE:
+                    choice		DB	'y'
+                    number		DW	12345
+                    neg_number	DW	-12345
+                    big_number	DQ	123456789
+                    real_number1	DD	1.234
+                    real_number2	DQ	123.456
+                    */
                 }
             }
         }
 
         public void Export(string Path)
         {
-            ASM.Add("\ntimes 510-($-$$) db 0");
-            ASM.Add("dw 0xAA55");
-            string Final = "";
+            //ASM["_."].Add("\ntimes 510-($-$$) db 0");
+            //ASM["_."].Add("dw 0xAA55");
+            string Final = "[ORG 0x7c00]\n";
+            Final+="[BITS 32]\n";
+            Final+="jmp Source.Kernel.Main\n\n";
             int k = 0;
-            foreach (string line in ASM)
+            foreach(KeyValuePair<string, List<string>> entry in ASM)
             {
-                Final += k++ == ASM.Count - 1 ? line : line + '\n';
+                if (entry.Key=="") {
+                    foreach(string poc in entry.Value) {
+                        Final+=$"{poc}\n";
+                    }
+                } else {
+
+                    Final+=$"{entry.Key}:\n";
+                    foreach(string poc in entry.Value) {
+                        Final+=$"{poc}\n";
+                    }
+                }
+                // do something with entry.Value or entry.Key
             }
+            /*foreach (string line in ASM)
+            {
+                Final += (k == ASM.Count - 1 ? line : line + '\n');
+                k++;
+            }*/
+            Final+="\ntimes 510-($-$$) db 0\n";
+            Final+="dw 0xAA55\n";
             File.WriteAllText(Path, Final);
         }
 
         #region OPCODES/METHODS
 
-        private void LoadString(MethodDef Method, Instruction Instruction)
-        {
-            try
-            {
-                ASM.Add($"\n{GetMethodName(Method)}.{Method.Body.Variables[VIndex]}:");
-                ASM.Add($"\t.size dd {Instruction.GetOperand().ToString().Length}");
-                ASM.Add($"\t.data db \"{Instruction.GetOperand()}\", 0");
-                ASM.Add($"\t.type db \"{Instruction.GetOperand().GetType()}\"");
+        private void LoadString(MethodDef Method, Instruction Instruction, int Index) {
+            Code[] codes={ Code.Stloc, Code.Stloc_0, Code.Stloc_1, Code.Stloc_2, Code.Stloc_3, Code.Stloc_S };
+            if (codes.Contains(Method.Body.Instructions[Index+1].GetOpCode().Code)) {
+                //it's a var
+                string vname=GetMethodName(Method) + "." + Method.Body.Variables[VIndex];
+                ASM.Add(vname, new());
+                ASM[vname].Add($"\t.size dd {Instruction.GetOperand().ToString().Length}");
+                ASM[vname].Add($"\t.data db \"{Instruction.GetOperand()}\", 0");
+                ASM[vname].Add($"\t.type db \"{Instruction.GetOperand().GetType()}\"");
                 VIndex++;
+            } else {
+                string vname=GetMethodName(Method) + ".S_" + SIndex;
+                ASM.Add(vname, new());
+                ASM[vname].Add($"\t.size dd {Instruction.GetOperand().ToString().Length}");
+                ASM[vname].Add($"\t.data db \"{Instruction.GetOperand()}\", 0");
+                ASM[vname].Add($"\t.type db \"{Instruction.GetOperand().GetType()}\"");
+                SIndex++;
             }
-            catch (ArgumentOutOfRangeException)
-            {
-                ASM.Add($"\n{GetMethodName(Method)}.S{VIndex}:");
-                ASM.Add($"\t.size dd {Instruction.GetOperand().ToString().Length}");
-                ASM.Add($"\t.data db \"{Instruction.GetOperand()}\", 0");
-                ASM.Add($"\t.type db \"{Instruction.GetOperand().GetType()}\"");
-                VIndex++;
-            }
-            //ASM.Add($"\t{Method.Body.Variables[VIndex]} db \"{Instruction.GetOperand()}\"");
+        }
+
+        private void Ret(MethodDef Method, Instruction Instruction, int Index) {
+            ASM[GetMethodName(Method)].Add("\tret");
         }
 
         #endregion
