10e9e7d43cc9523ba32b94e224892b286158edb7
Astrido <73673156+asterd-og@users.noreply.github.com>
2022-03-02 20:59:12 -0300

    Update Program.cs

--- Kernel/Program.cs
+++ Kernel/Program.cs
@@ -1,15 +1,10 @@
-﻿namespace Source
+namespace Source
 {
     public static class Kernel
     {
         public static void Main()
         {
-            if (true)
-            {
-                const string F = "Hello, World!";
-                const string D = "Hai :3";
-                Console.WriteLine(F + " " + D);
-            }
+            Console.WriteLine("Hello world from C#!");
         }
     }
 }
